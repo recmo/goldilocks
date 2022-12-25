224e7170aa0939b6f427bdbb252d924c16845cbc
Remco Bloemen <remco@wicked.ventures>
2022-12-24 20:20:21 -0800

    Add prefetching

--- src/algo/aarch64.rs
+++ src/algo/aarch64.rs
@@ -5,7 +5,9 @@ use core::arch::asm;
 // See <https://7-cpu.com/cpu/Apple_M1.html>
 // See <https://gist.github.com/recmo/389339a2e71825eadad1e56106b2e24b>
 
-//
+// TODO: Prefetching
+
+// TODO: Associativity issues? https://en.algorithmica.org/hpc/cpu-cache/associativity/
 
 /// Transpose swap two 4x4 matrices in memory.
 ///
@@ -14,13 +16,33 @@ use core::arch::asm;
 /// [  4  5  6  7]   /==\  [ 1  5  9 13]
 /// [  8  9 10 11]   \==/  [ 2  6 10 14]
 /// [ 12 13 14 15]         [ 3  7 11 15]
-pub unsafe fn transpose_swap_4(a: &mut [u64], b: &mut [u64], stride: usize) {
+pub unsafe fn transpose_swap_4(a: *mut u64, b: *mut u64, stride: usize) {
     asm!(
         r#"
-        dmb nshld ; Memory barrier
-
         ; Load a pair of registers with non-temporal hint. This loads 256 bits
         ; or 4 64-bit integers. Therefore we swap a 4x4 matrix at a time.
+
+        dmb nshld ; memory barrier to make sure addresses are loaded correctly
+                  ; see <https://developer.arm.com/documentation/den0024/a/Memory-Ordering/Barriers/Non-temporal-load-and-store-pair>
+        
+        ; Prefetch A matrix reads
+        prfm pldl1strm, [{a0}, #960]
+        prfm pldl1strm, [{a1}, #960]
+        prfm pldl1strm, [{a2}, #960]
+        prfm pldl1strm, [{a3}, #960]
+
+        ; Prefetch B matrix reads
+        add {temp}, {b0}, {b_prefetch}, lsl #3
+        prfm pldl1strm, [{temp}]
+
+        add {temp}, {b1}, {b_prefetch}, lsl #3
+        prfm pldl1strm, [{temp}]
+
+        add {temp}, {b2}, {b_prefetch}, lsl #3
+        prfm pldl1strm, [{temp}]
+
+        add {temp}, {b3}, {b_prefetch}, lsl #3
+        prfm pldl1strm, [{temp}]
 
         ; Read the whole first matrix
         ldnp q0, q1, [{a0}]  ; v0 = A[ 0  1]  v1 = A[ 2  3]
@@ -64,26 +86,44 @@ pub unsafe fn transpose_swap_4(a: &mut [u64], b: &mut [u64], stride: usize) {
         trn2.2d v6, v12, v14    ; v6 = B[ 6  7]
         stnp q4, q6, [{a1}]
 
-        trn1.2d v4,  v9, v11    ; v4 = B[ 0  1]
-        trn1.2d v6, v13, v15    ; v6 = B[ 3  3]
+        trn1.2d v4,  v9, v11    ; v4 = B[ 8  9]
+        trn1.2d v6, v13, v15    ; v6 = B[10 11]
         stnp q4, q6, [{a2}]
 
-        trn2.2d v4,  v9, v11    ; v4 = B[ 4  5]
-        trn2.2d v6, v13, v15    ; v6 = B[ 6  7]
+        trn2.2d v4,  v9, v11    ; v4 = B[12 13]
+        trn2.2d v6, v13, v15    ; v6 = B[14 15]
         stnp q4, q6, [{a3}]
-
-        dmb nshld ; Memory barrier
     "#,
-        a0 = in(reg) a.as_ptr(),
-        a1 = in(reg) a.as_ptr().add(stride),
-        a2 = in(reg) a.as_ptr().add(stride * 2),
-        a3 = in(reg) a.as_ptr().add(stride * 3),
-        b0 = in(reg) b.as_ptr(),
-        b1 = in(reg) b.as_ptr().add(stride),
-        b2 = in(reg) b.as_ptr().add(stride * 2),
-        b3 = in(reg) b.as_ptr().add(stride * 3),
+        a0 = in(reg) a,
+        a1 = in(reg) a.add(stride),
+        a2 = in(reg) a.add(stride * 2),
+        a3 = in(reg) a.add(stride * 3),
+        b0 = in(reg) b,
+        b1 = in(reg) b.add(stride),
+        b2 = in(reg) b.add(stride * 2),
+        b3 = in(reg) b.add(stride * 3),
+        b_prefetch = in(reg) 30 * 4 * stride,
+        temp = out(reg) _,
         options(nostack)
     )
+}
+
+pub fn transpose(a: &mut [u64], size: usize) {
+    assert_eq!(a.len(), size * size);
+    assert_eq!(size % 4, 0);
+
+    for i in (0..size).step_by(4) {
+        for j in (i..size).step_by(4).skip(1) {
+            let (a, b) = a.split_at_mut(size * size);
+            unsafe {
+                transpose_swap_4(
+                    a.as_mut_ptr().add(i * size + j),
+                    a.as_mut_ptr().add(j * size + i),
+                    size,
+                );
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -107,7 +147,7 @@ mod test {
             }
             println!("");
         }
-            println!("");
+        println!("");
         for i in 0..4 {
             for j in 0..4 {
                 print!("{:3} ", b[i * 4 + j]);
@@ -116,10 +156,10 @@ mod test {
             }
             println!("");
         }
-            println!("");
+        println!("");
 
         unsafe {
-            transpose_swap_4(&mut a, &mut b, 4);
+            transpose_swap_4(a.as_mut_ptr(), b.as_mut_ptr(), 4);
         }
 
         for i in 0..4 {
@@ -130,7 +170,7 @@ mod test {
             }
             println!("");
         }
-            println!("");
+        println!("");
         for i in 0..4 {
             for j in 0..4 {
                 print!("{:3} ", b[i * 4 + j]);
@@ -148,8 +188,52 @@ mod test {
 pub mod bench {
     use super::*;
     use core::hint::black_box;
-    use criterion::{BatchSize, Criterion};
+    use criterion::{BatchSize, Criterion, Throughput};
     use rand::{thread_rng, Rng};
 
-    pub fn group(criterion: &mut Criterion) {}
+    pub fn group(criterion: &mut Criterion) {
+        bench_transpose_4(criterion);
+        bench_transpose(criterion);
+    }
+
+    pub fn bench_transpose_4(c: &mut Criterion) {
+        let mut group = c.benchmark_group("transpose/aarch64/transpose_swap_4");
+        let size = 4;
+        group.throughput(Throughput::Elements(4));
+        group.bench_function(format!("4x4"), |b| {
+            b.iter_batched_ref(
+                || {
+                    (
+                        (0..16).map(|_| thread_rng().gen()).collect::<Vec<u64>>(),
+                        (0..16).map(|_| thread_rng().gen()).collect::<Vec<u64>>(),
+                    )
+                },
+                |(a, b)| unsafe { transpose_swap_4(a.as_mut_ptr(), b.as_mut_ptr(), 4) },
+                BatchSize::LargeInput,
+            )
+        });
+        group.finish();
+    }
+
+    pub fn bench_transpose(c: &mut Criterion) {
+        let mut group = c.benchmark_group("transpose/aarch64");
+        let max = if cfg!(test) { 5 } else { 16 };
+        for i in 5..=max {
+            let size = 1_usize << i;
+            group.throughput(Throughput::Elements((size * size) as u64));
+            group.sample_size(if i < 10 { 100 } else { 10 });
+            group.bench_function(format!("{size}x{size}"), |b| {
+                b.iter_batched_ref(
+                    || {
+                        (0..size * size)
+                            .map(|_| thread_rng().gen())
+                            .collect::<Vec<u64>>()
+                    },
+                    |m| transpose(m.as_mut_slice(), size),
+                    BatchSize::LargeInput,
+                )
+            });
+        }
+        group.finish();
+    }
 }
