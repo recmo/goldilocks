93d9531f39eaebc0d2d1d6ada39d81c5633ba0fd
Remco Bloemen <remco@wicked.ventures>
2023-01-13 12:39:51 -0800

    Good-Thomas 15 demo

--- src/ntt/good_thomas.rs
+++ src/ntt/good_thomas.rs
@@ -12,47 +12,31 @@ pub fn recurse(value: &mut [Field], inner: impl Fn(&mut [Field]), (a, b): (usize
 
 pub fn ntt_15(values: &mut [Field]) {
     debug_assert_eq!(values.len(), 15);
+    let n = 15;
     let (a, b) = (3, 5);
-
-    let mut expected = values.to_vec();
-    crate::ntt::naive::ntt(&mut expected);
+    let (n1, n2) = (5, 3);
+    let (m1, m2) = (10, 6);
 
     // Input permutation.
-    let mut buffer = [
-        values[0], values[6], values[12], values[3], values[9],
-        values[10], values[1], values[7], values[13], values[4],
-        values[5], values[11], values[2], values[8], values[14],
-    ];
-    // buffer[0] = values[0];
-    // buffer[6] = values[1];
-    // buffer[12] = values[2];
-    // buffer[3] = values[3];
-    // buffer[9] = values[4];
-    // buffer[10] = values[5];
-    // buffer[1] = values[6];
-    // buffer[7] = values[7];
-    // buffer[13] = values[8];
-    // buffer[4] = values[9];
-    // buffer[5] = values[10];
-    // buffer[11] = values[11];
-    // buffer[2] = values[12];
-    // buffer[8] = values[13];
-    // buffer[14] = values[14];
+    let mut buffer = [Field::new(0); 15];
+    for i1 in 0..a {
+        for i2 in 0..b {
+            let i = (i1 * n1 + i2 * n2) % n;
+            buffer[i1 * b + i2] = values[i];
+        }
+    }
 
-    transpose(&mut buffer, (a, b));
-    buffer.chunks_exact_mut(a).for_each(small::ntt_3);
-    transpose(&mut buffer, (b, a));
     buffer.chunks_exact_mut(b).for_each(small::ntt_5);
-    transpose(&mut buffer, (a, b));
+    transpose(&mut buffer, (b, a));
+    buffer.chunks_exact_mut(a).for_each(small::ntt_3);
 
     // Output permutation.
-    values.copy_from_slice(&[
-        buffer[0], buffer[3], buffer[6], buffer[9], buffer[12],
-        buffer[5], buffer[8], buffer[11], buffer[14], buffer[2],
-        buffer[10], buffer[13], buffer[1], buffer[4], buffer[7],
-    ]);
-
-    assert_eq!(values, &expected[..]);
+    for i1 in 0..a {
+        for i2 in 0..b {
+            let k = (i1 * m1 + i2 * m2) % n;
+            values[k] = buffer[i1 + i2 * a];
+        }
+    }
 }
 
 #[cfg(test)]
